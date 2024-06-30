50a4535fa08db44e3a6d3c8e8eef0ccd92a7f0c9
ikalakhi <imranekalakhi13@gmail.com>
2024-06-30 11:18:40 +0100

    adding the Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return(
+        <div>
+            <header id="header">
+                <img src="/investment-calculator-logo.png" alt="investment calculator logo"/>
+                <h1>Investment Calculator</h1>
+            </header>
+        </div>
+    );
+}

8bdd70cae34a2b59718135a2aa64c90f897d78c2
ikalakhi <imranekalakhi13@gmail.com>
2024-06-30 12:20:22 +0100

    Handling Events & Using Two-Way-Binding

--- src/components/userInput.jsx
+++ src/components/userInput.jsx
@@ -1,24 +1,63 @@
+import { useState } from "react";
+
 export default function UserInput(){
+    const [userInput, setUserInput] = useState({
+        initialInvestment: 10000,
+        annualInvestment: 1200,
+        expectedReturn: 6,
+        duration: 10
+    });
+
+    function handelChange(inputIdentifier, newValue) {
+        setUserInput(prevUserInput => {
+            return {
+                ...prevUserInput,
+                [inputIdentifier]: newValue
+            }
+        })
+    }
+    console.log(userInput);
+
     return(
         <section id="user-input">
             <div className="input-group">
                 <p>
                     <label htmlFor="initial_investment">INITIAL INVESTMENT</label>
-                    <input type="number" required defaultValue="0"/>
+                    <input 
+                        type="number" 
+                        required
+                        value={userInput.initialInvestment}
+                        onChange={(e) => handelChange("initialInvestment", e.target.value)}
+                    />
                 </p>
                 <p>
                     <label htmlFor="annual-investment">ANNUAL INVESTMENT</label>
-                    <input type="number" required defaultValue="0"/>
+                    <input 
+                        type="number" 
+                        required
+                        value={userInput.annualInvestment}
+                        onChange={(e) => handelChange("annualInvestment", e.target.value)}
+                    />
                 </p>
             </div>
             <div className="input-group">
                 <p>
                     <label htmlFor="expected-return">EXPECTED RETURN</label>
-                    <input type="number" required defaultValue="0"/>
+                    <input 
+                        type="number" 
+                        required
+                        value={userInput.expectedReturn}
+                        onChange={(e) => handelChange("expectedReturn", e.target.value)}
+                    />
                 </p>
                 <p>
                     <label htmlFor="duration">DURATION</label>
-                    <input type="number" required defaultValue="0"/>
+                    <input 
+                        type="number" 
+                        required
+                        value={userInput.duration}
+                        onChange={(e) => handelChange("duration", e.target.value)}
+                    />
                 </p>
             </div>
         </section>

8d31bb1e5ed5b3832537b0548a9f1573cdac7683
ikalakhi <imranekalakhi13@gmail.com>
2024-06-30 11:42:17 +0100

    changes in UserInput component

--- src/components/userInput.jsx
+++ src/components/userInput.jsx
@@ -1,16 +1,26 @@
 export default function UserInput(){
     return(
-        <div id="user-input">
-            <form className="input-group">
-                <label htmlFor="initial_investment">INITIAL INVESTMENT</label>
-                <input type="text" required defaultValue="0"/>
-                <label htmlFor="annual-investment">ANNUAL INVESTMENT</label>
-                <input type="text" required defaultValue="0"/>
-                <label htmlFor="expected-return">EXPECTED RETURN</label>
-                <input type="text" required defaultValue="0"/>
-                <label htmlFor="duration">DURATION</label>
-                <input type="text" required defaultValue="0"/>
-            </form>
-        </div>
+        <section id="user-input">
+            <div className="input-group">
+                <p>
+                    <label htmlFor="initial_investment">INITIAL INVESTMENT</label>
+                    <input type="number" required defaultValue="0"/>
+                </p>
+                <p>
+                    <label htmlFor="annual-investment">ANNUAL INVESTMENT</label>
+                    <input type="number" required defaultValue="0"/>
+                </p>
+            </div>
+            <div className="input-group">
+                <p>
+                    <label htmlFor="expected-return">EXPECTED RETURN</label>
+                    <input type="number" required defaultValue="0"/>
+                </p>
+                <p>
+                    <label htmlFor="duration">DURATION</label>
+                    <input type="number" required defaultValue="0"/>
+                </p>
+            </div>
+        </section>
     );
 }
